7e19c8034b82e2123704da01595d22fabed77160
hunghsinke <78602893+hunghsinke@users.noreply.github.com>
2023-10-02 08:28:42 +0000

    Sort array practice, add "Kiwi" to the end of the array

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 // using this array,
 var array = ["Banana", "Apples", "Oranges", "Blueberries"];
 // 1. Remove the Banana from the array.
-var newArray = array.shift();
-console.log(newArray);// this will show Banana, because that's what we shift out of the array
-console.log(array);//this will show "Apples", "Oranges", "Blueberries"
+// var newArray = array.shift();
+// console.log(newArray);// this will show Banana, because that's what we shift out of the array
+// console.log(array);//this will show "Apples", "Oranges", "Blueberries"
 // 2. Sort the array in order.
+// var newArray = array.sort();
+// console.log(newArray);
 
 // 3. Put "Kiwi" at the end of the array.
+var newArray = array.concat("Kiwi");
+console.log(newArray);
 
 // 4. Remove "Apples" from the array.
 
 // 5. Sort the array in reverse order. (Not alphabetical, but reverse
 // the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
 
-you should have at the end:
-["Kiwi", "Oranges", "Blueberries"]
+// you should have at the end:
+// ["Kiwi", "Oranges", "Blueberries"]
 
 // using this array,
 // var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
